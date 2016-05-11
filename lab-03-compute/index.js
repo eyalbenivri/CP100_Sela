67da4821e7f87d820a9f3b94e4df0cd9becad2fc
Eyal Ben Ivri <eyalbenivri@gmail.com>
2016-05-11 11:30:23 +0300

    forgot commas

--- lab-03-compute/index.js
+++ lab-03-compute/index.js
@@ -2,9 +2,9 @@ var Twitter = require('twitter');
 var config = require('./config');
 
 var client = new Twitter({
-  consumer_key:         config.get("TWITTER_CONSUMER_KEY")
-  consumer_secret:      config.get("TWITTER_CONSUMER_SECRET")
-  access_token_key:     config.get("TWITTER_ACCESS_TOKEN_KEY")
+  consumer_key:         config.get("TWITTER_CONSUMER_KEY"),
+  consumer_secret:      config.get("TWITTER_CONSUMER_SECRET"),
+  access_token_key:     config.get("TWITTER_ACCESS_TOKEN_KEY"),
   access_token_secret: 	config.get("TWITTER_ACCESS_TOKEN_SECRET")
 });
 
